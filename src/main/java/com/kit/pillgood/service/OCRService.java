2f480e33431a67c55a12c9556b3ab72e1a526f02
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2023-05-20 22:19:50 +0900

    전처리완료

--- src/main/java/com/kit/pillgood/service/OCRService.java
+++ src/main/java/com/kit/pillgood/service/OCRService.java
@@ -17,71 +17,61 @@ public class OCRService {
     }
     public void extractPrescriptionTextFromImage() {
 
-//        String filePath = "C:/Users/lenovo/Pictures/OCRTest/Original.jpg";
-//
-//        Mat image = Imgcodecs.imread(filePath);
-//
-//        Mat grayImage = new Mat();
-//
-//        Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
-//
-//        Imgcodecs.imwrite("C:/Users/lenovo/Pictures/OCRTest/GrayScale.png", grayImage);
-//
-//        Mat binaryImage1 = new Mat();
-//        Imgproc.adaptiveThreshold(
-//                grayImage,                        // 입력 이미지
-//                binaryImage1,                  // 결과 이미지
-//                255,                          // 이진화 값 (흰색)
-//                Imgproc.ADAPTIVE_THRESH_MEAN_C,// 적응적 이진화 방법 (평균)
-//                Imgproc.THRESH_BINARY,        // 이진화 방법 (흰색/검정색)
-//                467,                           // 블록 크기 (홀수)
-//                60                           // 평균값에서 차감할 값
-//        );
-//
-//        Imgcodecs.imwrite("C:/Users/lenovo/Pictures/OCRTest/AdaptiveBinary.png", binaryImage1);
-//
-//        Mat binaryImage2 = new Mat();
-//        Imgproc.threshold(grayImage, binaryImage2, 0, 255, Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);
-//
-//        Imgcodecs.imwrite("C:/Users/lenovo/Pictures/OCRTest/GlobalBinary.png", binaryImage2);
-//
-//        Mat equalizedImage = new Mat();
-//        Imgproc.equalizeHist(binaryImage1, equalizedImage);
-//
-//        Imgcodecs.imwrite("C:/Users/lenovo/Pictures/OCRTest/Hist.png", equalizedImage);
-//
-//        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
-//        Mat denoisedImage = new Mat();
-//        Imgproc.morphologyEx(equalizedImage, denoisedImage, Imgproc.MORPH_OPEN, kernel);
-//
-//        // 결과 이미지 저장
-//        Imgcodecs.imwrite("C:/Users/lenovo/Pictures/OCRTest/Mopology.png", denoisedImage);
-//
-//        Mat BlurImage = new Mat();
-//        Imgproc.GaussianBlur(equalizedImage, BlurImage, new Size(3, 3), 0);
-//
-//        Imgcodecs.imwrite("C:/Users/lenovo/Pictures/OCRTest/Blur.png", BlurImage);
-
-//        double threshold1 = 50;
-//        double threshold2 = 150;
-//        int apertureSize = 3;
-//        boolean L2gradient = false;
-//
-//        // 케니 엣지 검출 수행
-//        Mat edges = new Mat();
-//        Imgproc.Canny(denoisedImage, edges, threshold1, threshold2, apertureSize, L2gradient);
-//
-//        Imgcodecs.imwrite("C:/Users/lenovo/Pictures/OCRTest/result3.png", edges);
-
+                imagePreProcess();
 
                 Tesseract tesseract = new Tesseract();
-                tesseract.setLanguage("kor");
+                tesseract.setLanguage("eng+kor");
                 tesseract.setDatapath("C:/Users/lenovo/Desktop/Tess4J/tessdata");
                 try {
-                    String result = tesseract.doOCR(new File("C:/Users/lenovo/Pictures/OCRTest/Test.png"));
+                    String result = tesseract.doOCR(new File("C:/Users/lenovo/Pictures/OCRTest/Blur.png"));
                     System.out.println(result);
                 } catch (TesseractException e) {
                     e.printStackTrace();
                 }
+    }
+
+    public void imagePreProcess() {
+        String filePath = "C:/Users/lenovo/Pictures/OCRTest/Original.jpg";
+
+        Mat image = Imgcodecs.imread(filePath);
+
+        // 그레이스케일
+        Mat grayImage = new Mat();
+        Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
+
+        Imgcodecs.imwrite("C:/Users/lenovo/Pictures/OCRTest/GrayScale.png", grayImage);
+
+        // 히스토그램 평활화
+        Mat equalizedImage = new Mat();
+        Imgproc.equalizeHist(grayImage, equalizedImage);
+
+        Imgcodecs.imwrite("C:/Users/lenovo/Pictures/OCRTest/Hist.png", equalizedImage);
+
+        // 적응적 이진화
+        Mat binaryImage1 = new Mat();
+        Imgproc.adaptiveThreshold(
+                grayImage,                        // 입력 이미지
+                binaryImage1,                  // 결과 이미지
+                255,                          // 이진화 값 (흰색)
+                Imgproc.ADAPTIVE_THRESH_MEAN_C,// 적응적 이진화 방법 (평균)
+                Imgproc.THRESH_BINARY,        // 이진화 방법 (흰색/검정색)
+                467,                           // 블록 크기 (홀수)
+                60                           // 평균값에서 차감할 값
+        );
+
+        Imgcodecs.imwrite("C:/Users/lenovo/Pictures/OCRTest/AdaptiveBinary.png", binaryImage1);
+
+        // 팽창 모폴로지
+        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2));
+        Mat denoisedImage = new Mat();
+        Imgproc.morphologyEx(binaryImage1, denoisedImage, Imgproc.MORPH_ERODE, kernel);
+
+        Imgcodecs.imwrite("C:/Users/lenovo/Pictures/OCRTest/Mopology.png", denoisedImage);
+
+        // 블러
+        Mat BlurImage = new Mat();
+        Imgproc.GaussianBlur(denoisedImage, BlurImage, new Size(3, 3), 0);
+
+        Imgcodecs.imwrite("C:/Users/lenovo/Pictures/OCRTest/Blur.png", BlurImage);
     }
 }
