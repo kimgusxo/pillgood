e266aee335480f686dd8a4ac37ab0a5e6600cec0
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2023-05-20 18:45:43 +0900

    오타수정

--- src/main/java/com/kit/pillgood/persistence/dto/LoginDTO.java
+++ src/main/java/com/kit/pillgood/persistence/dto/LoginDTO.java
@@ -12,7 +12,7 @@ public class LoginDTO {
 
     @NotEmpty(groups = {ValidationGroups.groupSearch.class}, message = "정확한 형식의 이메일 값을 입력하세요")
     @Email(groups = {ValidationGroups.groupSearch.class})
-    private String userEamil;
+    private String userEmail;
 
     @NotEmpty(groups = {ValidationGroups.groupSearch.class}, message = "토큰 값은 필수 입니다.")
     private String userToken;
