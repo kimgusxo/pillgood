c9c381a1d278eb83f0322471dbfb5cffc4f2ce09
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2023-06-03 19:38:33 +0900

    쿼리 에러 해결

--- src/main/java/com/kit/pillgood/repository/TakePillCheckRepository.java
+++ src/main/java/com/kit/pillgood/repository/TakePillCheckRepository.java
@@ -5,6 +5,7 @@ import com.kit.pillgood.persistence.dto.NotificationContentDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -15,6 +16,6 @@ public interface TakePillCheckRepository  extends JpaRepository<TakePillCheck, L
 
     @Modifying
     @Query("UPDATE TakePillCheck tpc SET tpc.takeCheck = :takeCheck WHERE tpc.takePillCheckIndex = :takePillCheckIndex")
-    void updateTakeCheck(Long TakePillIndex, Boolean takeCheck);
+    void updateTakeCheck(@Param("takePillCheckIndex") Long takePillCheckIndex, @Param("takeCheck") Boolean takeCheck);
 
 }
